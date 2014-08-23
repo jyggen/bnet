79970befd5177f94c3d3f02adcba1f9b5bce8f15
Jonas Stendahl <jonas.stendahl@gmail.com>
2014-08-23 17:48:31 +0200

    100% Region tests.

--- test/RegionTest.php
+++ test/RegionTest.php
@@ -7,8 +7,33 @@ class RegionTest extends \PHPUnit_Framework_TestCase
 {
     public function testRegion()
     {
+        $region = new Region(Region::EUROPE, 'fr_FR');
+        $this->assertSame('fr_FR', $region->getLocale());
+        $this->assertSame('https://eu.api.battle.net/wow/', $region->getHost('wow'));
+    }
+
+    public function testDefaultLocale()
+    {
         $region = new Region(Region::EUROPE);
         $this->assertSame('en_GB', $region->getLocale());
         $this->assertSame('https://eu.api.battle.net/wow/', $region->getHost('wow'));
+    }
+
+    /**
+     * @expectedException        OutOfBoundsException
+     * @expectedExceptionMessage invalid is not a valid region.
+     */
+    public function testInvalidRegion()
+    {
+        new Region('invalid');
+    }
+
+    /**
+     * @expectedException        OutOfBoundsException
+     * @expectedExceptionMessage invalid is not a valid locale.
+     */
+    public function testInvalidLocale()
+    {
+        new Region(Region::EUROPE, 'invalid');
     }
 }
