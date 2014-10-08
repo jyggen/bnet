8531d11404cd66e694a46c56cc9b24446317ebb2
Jonas Stendahl <jonas.stendahl@gmail.com>
2014-10-08 14:39:01 +0200

    Refactored Utility::realmNameToSlug() to not require patchwork/utf8.

--- src/Utility.php
+++ src/Utility.php
@@ -5,13 +5,18 @@ use Patchwork\Utf8;
 
 class Utility
 {
+    protected static $replaceTable = [
+        ['à', 'ê', 'é', 'ü'],
+        ['a', 'e', 'e', 'u'],
+    ];
+
     public static function realmNameToSlug($realmName)
     {
-        $slug = Utf8::toAscii($realmName);
+        $slug = str_replace(static::$replaceTable[0], static::$replaceTable[1], $realmName);
         $slug = str_replace(['-'], [''], $slug);
-        $slug = preg_replace('![^'.preg_quote('-').'\pL\pN\s]+!u', '', mb_strtolower($slug));
+        $slug = preg_replace('![^'.preg_quote('-').'\pL\pN\s]+!u', '', mb_strtolower($slug, 'UTF-8'));
         $slug = preg_replace('!['.preg_quote('-').'\s]+!u', '-', $slug);
-        $slug =  trim($slug, '-');
+        $slug = trim($slug, '-');
 
         if ($slug === '') {
             return str_replace([' '], ['-'], $realmName);
