a9c077040c1c5efee581faf8a1e59df0a40d1aa3
Jonas Stendahl <jonas.stendahl@gmail.com>
2014-12-27 16:09:05 +0100

    Error handler for weird Battle.net issue.

--- src/Warcraft/Auctions/AuctionRequest.php
+++ src/Warcraft/Auctions/AuctionRequest.php
@@ -8,10 +8,20 @@ class AuctionRequest extends AbstractRequest
 {
     public function download(IndexEntity $index)
     {
-        $data     = json_decode(file_get_contents($index->url), true);
+        $data = file_get_contents($index->url);
+
+        if ($data === false) {
+            throw new BattleNetException('Unable to download auction dump from "'.$index->url.'"');
+        }
+
+        $json     = json_decode($data, true);
         $auctions = [];
 
-        foreach ($data['auctions']['auctions'] as $auction) {
+        if (is_array($json) === false) {
+            throw new BattleNetException('Invalid json returned in auction dump from "'.$index->url.'"');
+        }
+
+        foreach ($json['auctions']['auctions'] as $auction) {
             $auctions[] = new AuctionEntity($auction);
         }
 
