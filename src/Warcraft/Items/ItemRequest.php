fb77532904143dfae9e37839c10d71e8d00964f4
Jonas Stendahl <jonas.stendahl@gmail.com>
2015-02-28 00:55:33 +0100

    Added support for item contexts and bonus lists.

--- src/Warcraft/Items/ItemRequest.php
+++ src/Warcraft/Items/ItemRequest.php
@@ -5,6 +5,8 @@ use Pwnraid\Bnet\Core\AbstractRequest;
 
 class ItemRequest extends AbstractRequest
 {
+    protected $context = null;
+
     public function classes()
     {
         $data = $this->client->get('data/item/classes');
@@ -12,13 +14,19 @@ class ItemRequest extends AbstractRequest
         return new ClassEntity($data);
     }
 
-    public function find($itemId)
+    public function find($itemId, $bonusList = [])
     {
-        $data = $this->client->get('item/'.$itemId);
+        $data = $this->client->get(rtrim('item/'.$itemId.'/'.$this->context, '/'), [
+            'query' => [
+                'bl' => implode(',', $bonusList),
+            ],
+        ]);
 
         if ($data === null) {
             return null;
         }
+
+        $this->context = '';
 
         return new ItemEntity($data);
     }
@@ -32,5 +40,12 @@ class ItemRequest extends AbstractRequest
         }
 
         return new ItemSetEntity($data);
+    }
+
+    public function withContext($context)
+    {
+        $this->context = $context;
+
+        return $this;
     }
 }
--- test/Warcraft/Items/ItemRequestTest.php
+++ test/Warcraft/Items/ItemRequestTest.php
@@ -50,6 +50,57 @@ class ItemRequestTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @covers ::find
+     * @uses   \Pwnraid\Bnet\Core\AbstractEntity
+     * @uses   \Pwnraid\Bnet\Core\AbstractRequest
+     */
+    public function testFindOnContextItem()
+    {
+        $request  = new ItemRequest(new TestClient('wow'));
+        $response = $request->find(110050);
+
+        $this->assertInstanceOf('\Pwnraid\Bnet\Warcraft\Items\ItemEntity', $response);
+        $this->assertSame(110050, $response->id);
+        $this->assertNull($response->name);
+    }
+
+    /**
+     * @covers ::find
+     * @covers ::withContext
+     * @uses   \Pwnraid\Bnet\Core\AbstractEntity
+     * @uses   \Pwnraid\Bnet\Core\AbstractRequest
+     */
+    public function testFindOnContextItemWithContext()
+    {
+        $request  = new ItemRequest(new TestClient('wow'));
+        $response = $request->withContext('dungeon-level-up-1')->find(110050);
+
+        $this->assertInstanceOf('\Pwnraid\Bnet\Warcraft\Items\ItemEntity', $response);
+        $this->assertSame(110050, $response->id);
+        $this->assertSame('Dagger of the Sanguine Emeralds', $response->name);
+        $this->assertSame('dungeon-level-up-1', $response->context);
+    }
+
+    /**
+     * @covers ::find
+     * @covers ::withContext
+     * @uses   \Pwnraid\Bnet\Core\AbstractEntity
+     * @uses   \Pwnraid\Bnet\Core\AbstractRequest
+     */
+    public function testFindOnContextItemWithBonusListWithContext()
+    {
+        $request  = new ItemRequest(new TestClient('wow'));
+        $response = $request->withContext('dungeon-level-up-1')->find(110050, [40, 41]);
+
+        $this->assertInstanceOf('\Pwnraid\Bnet\Warcraft\Items\ItemEntity', $response);
+        $this->assertSame(110050, $response->id);
+        $this->assertSame('Dagger of the Sanguine Emeralds', $response->name);
+        $this->assertSame('dungeon-level-up-1', $response->context);
+        $this->assertTrue(in_array(40, $response->bonusLists));
+        $this->assertTrue(in_array(41, $response->bonusLists));
+    }
+
+    /**
      * @covers ::findSet
      * @uses   \Pwnraid\Bnet\Core\AbstractEntity
      * @uses   \Pwnraid\Bnet\Core\AbstractRequest
