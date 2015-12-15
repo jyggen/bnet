9436847efefb25797ac8aa430917781457a0ab7c
Michael Lundbøl <michael.lundboel@gmail.com>
2015-12-15 23:25:33 +0100

    Fix psr-2 code style

--- src/Warcraft/Guilds/GuildEntity.php
+++ src/Warcraft/Guilds/GuildEntity.php
@@ -6,12 +6,13 @@ use Pwnraid\Bnet\Warcraft\Characters\CharacterEntity;
 
 class GuildEntity extends AbstractEntity
 {
-    public function __construct(array $body) {
+    public function __construct(array $body)
+    {
 
         parent::__construct($body);
 
-        if(array_key_exists('members', $this->attributes) === true) {
-            foreach($this->attributes['members'] as $key => $member) {
+        if (array_key_exists('members', $this->attributes) === true) {
+            foreach ($this->attributes['members'] as $key => $member) {
                 $this->attributes['members'][$key]['character'] = new CharacterEntity($member);
             }
         }
